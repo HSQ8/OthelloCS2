9705ed0c1eb4819e7c3ad4a8b4cb51630d6efc0b
cs2user <danineamati@gmail.com>
2018-03-07 16:58:08 -0800

    testing

--- player.cpp
+++ player.cpp
@@ -58,12 +58,12 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
     // First we need to update the board.
     playerboard->doMove(opponentsMove, oppSide);
 
-    if (msLeft > 100000) {
-        return doMiniMax(3);
+    /*if (msLeft > 100000) {
+        return doMiniMax(4);
     }
 
     else if (msLeft > 50000) {
-        return doMiniMax(2);
+        return doMiniMax(3);
     }
 
     else if (msLeft > 25000) {
@@ -74,7 +74,7 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
         return doMiniMax(1);
     }
 
-    else if (msLeft > 10000) {
+    else*/ if (msLeft > 10000) {
         return doSimpleHeuristicMove();
     }
 
